77fdbd477b5fdb9f83e87f26b891f58444711ed0
Amber Turner <turneram@my.lanecc.edu>
2020-10-07 08:56:51 -0700

    site beginning

---
+++ Controllers/ReferencesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AmberTurnerSite.Controllers
+{
+    public class ReferencesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
