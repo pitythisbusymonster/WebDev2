a08aff981930e94b7635ba8eb3271eb39921dcca
Amber Turner <turneram@my.lanecc.edu>
2020-11-09 10:16:14 -0800

    started lab6

--- AmberTurnerSite/Controllers/HomeController.cs
+++ AmberTurnerSite/Controllers/HomeController.cs
@@ -6,18 +6,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AmberTurnerSite.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AmberTurnerSite.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        /*private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
+        }*/
 
+        ForumContext context;//
+        public HomeController(ForumContext c)//
+        {
+            context = c;
+        }
         
         public IActionResult Index()
         {
@@ -39,8 +45,21 @@ namespace AmberTurnerSite.Controllers
         public IActionResult Forum(Forum model)
         {
             model.PostDate = DateTime.Now;
+            //store the model in the db
+            context.Forum.Add(model);//
+            context.SaveChanges();
+
             return View(model);
         }
+
+        /*public IActionResult Forums()//
+        {
+            //was "working" before inclusion of "include(...)" but wasn't writing to screen
+            //need to figure out proper Include() pm, lamda expression
+            //var forumPosts = context.Forum.Include(post => ).ToList<Forum>();  //if errors, dblck if <forum> is right
+
+            return View(forumPosts);
+        }*/
 
         public IActionResult Overview()
         {
