4d443aef5a56dd92beeb49b2e2c902b3a7897beb
Amber Turner <turneram@my.lanecc.edu>
2021-01-07 15:35:10 -0800

    added validation and migrations

--- AmberTurnerSite/Models/Forum.cs
+++ AmberTurnerSite/Models/Forum.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AmberTurnerSite.Models
 {
@@ -10,10 +11,21 @@ namespace AmberTurnerSite.Models
     {
         //these become foreign keys, for db
         public int ForumID {get; set;} //
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, MinimumLength = 3,
+            ErrorMessage = "Title must be between 3 and 100 characters")]
         public string PageName { get; set; }
+
         public string PageRating { get; set; }
+
+        [Required] 
         public string PostText { get; set; }
+
+        [Required(ErrorMessage = "Poster name is required")]
+        [StringLength(100, MinimumLength = 3)]
         public User PostCreator { get; set; }
+
         public DateTime PostDate { get; set; }
 
         
--- AmberTurnerSite/Models/User.cs
+++ AmberTurnerSite/Models/User.cs
@@ -3,12 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AmberTurnerSite.Models
 {
     public class User       //all is exact same as Brian's
     {
         public int UserID { get; set; } //
+
+        [StringLength(100, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
     }
 }
